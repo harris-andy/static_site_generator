d49fb34e1688fa2b4d80cbcf84e1acb2a6eabe11
harris-andy <agharris@gmail.com>
2024-07-15 18:03:42 -0400

    B: add setup files

---
+++ src/textnode.py
@@ -0,0 +1,23 @@
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other_node):
+        return (
+            self.text == other_node.text
+            and self.text_type == other_node.text_type
+            and self.url == other_node.url
+        )
+    
+    def __repr__(self) -> str:
+        print(f"TextNode({self.text}, {self.text_type}, {self.url})")
+
+
+def main():
+    text_test = TextNode("fuck this", "bold", "www.fuckoff.com")
+    text_test.__repr__()
+
+
+main()
