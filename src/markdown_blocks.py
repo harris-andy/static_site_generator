e65efb9d26e435b9a063b59db0f57cb73ffa55b1
harris-andy <agharris@gmail.com>
2024-08-01 15:15:21 -0400

    added some bullshit I don't care about (ch. 4.2)

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,4 +1,11 @@
 
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_olist = "ordered_list"
+block_type_ulist = "unordered_list"
+
 text_block = """
 # This is a heading
 
@@ -20,6 +27,40 @@ def markdown_to_blocks(markdown):
 
 markdown_to_blocks(text_block)
 
+def block_to_block_type(block):
+    lines = block.split("\n")
 
-def block_to_block_type(text_block):
-    ...
+    if (
+        block.startswith("# ")
+        or block.startswith("## ")
+        or block.startswith("### ")
+        or block.startswith("#### ")
+        or block.startswith("##### ")
+        or block.startswith("###### ")
+    ):
+        return block_type_heading
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return block_type_code
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return block_type_paragraph
+        return block_type_quote
+    if block.startswith("* "):
+        for line in lines:
+            if not line.startswith("* "):
+                return block_type_paragraph
+        return block_type_ulist
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return block_type_paragraph
+        return block_type_ulist
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return block_type_paragraph
+            i += 1
+        return block_type_olist
+    return block_type_paragraph
